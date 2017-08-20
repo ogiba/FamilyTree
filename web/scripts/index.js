d06fb3a90720dbe1d341cf928955e0435c26d299
Robert Ogiba <robert.ogiba@outlook.com>
2017-08-20 22:09:34 +0200

    Implemented scroll listener responsible for anchoring navbar to top when detect that it touches top of the window

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -2,6 +2,23 @@
  * Created by ogiba on 20.08.2017.
  */
 
+window.addEventListener("DOMContentLoaded", function () {
+    setupScrollListener();
+});
+
+function setupScrollListener() {
+    var navBar =  $('.nav-bar');
+    var distance = navBar.offset().top;
+
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() >= distance ) {
+            navBar.addClass('attach-top');
+        } else {
+            navBar.removeClass("attach-top");
+        }
+    });
+}
+
 function selectPage(pageNumber) {
     console.log(pageNumber);
     $.get("/page/" + pageNumber, function (data) {
