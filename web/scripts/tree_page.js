b739f4e8f16f82ed0c5a7a17aa162ab340375afb
Robert Ogiba <robert.ogiba@outlook.com>
2017-09-11 21:46:39 +0200

    Changed methods responsible for drag and drop person items

--- web/scripts/tree_page.js
+++ web/scripts/tree_page.js
@@ -4,94 +4,32 @@
 
 $(document).ready(function () {
     $("#playground").css("padding-top", "" + $(".nav-bar").height() + "px");
-
-    setupDraggable()
 });
 
-function setupDraggable() {
-
-
-    // $("#draggableTest").draggable({
-    //     scroll: true,
-    //     containment: '#treeContainer',
-    //     drag: function () {
-    //         var offset = $(this).offset();
-    //         var xPos = offset.left;
-    //         var yPos = offset.top;
-    //         console.log("x: " + xPos);
-    //         console.log("y: " + yPos);
-    //     }
-    // });
-
-    $(".draggableTest").draggable({
-        scope: 'demoBox',
-        containment: "document",
-        revertDuration: 100,
-        appendTo: "#playground",
-        helper: "clone",
-        connectToSortable: ".person-container ul",
-        cursorAt: {left: 5},
-        revert: function (droppableObj) {
-            if (droppableObj === false) {
-                $(this).removeClass('dragging');
-                return true;
-            }
-            else {
-                return false;
-            }
-        },
-        start: function (event, ui) {
-            //Reset
-
-            $(".draggableTest").draggable({
-                revert: true
-            });
-
-            $(this).css("display", "none");
-
-            $draggingParent = $(this).parent();
-            $(this).addClass('dragging');
-        }
-    });
-
-    $(".tree-container").droppable({
-        scope: 'demoBox',
-        drop: function (event, ui) {
-            var box = $(ui.draggable).html();
-            $(".draggableTest").draggable("option", "revert", false);
-
-            //Realign item
-            $(ui.draggable).detach().css({top: 0, left: 0, display: "inline-block"}).appendTo(this);
-        }
-    })
-
-    $(".person-container ul").droppable({
-        scope: 'demoBox',
-        placeholder: "ui-state-highlight",
-        drop: function (event, ui) {
-            $(".draggableTest").draggable("option", "revert", false);
-
-            $(ui.draggable).detach().css({top: "", left: "", display: "block"}).appendTo(this);
-        }
-        }).sortable({
-            stop: function (event, ui) {
-            $(ui.item).removeClass('dragging');
-            $('.dragging').remove();
-            if ($(this).hasClass('new')) {
-                var clone = $(this).clone();
-                clone.empty();
-                $(this).after(clone);
-                $(this).removeClass('new');
-            }
-            cleanUp();
-        }
-    });
+function allowDrop(ev) {
+    ev.preventDefault();
 }
 
-function cleanUp() {
-    $('.droppable').not('.new').each(function () {
-        if ($(this).find('li').length === 0) {
-            $(this).remove();
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function makePlace(ev) {
+    ev.preventDefault();
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    var targetItem = ev.target;
+    var parentList = targetItem.parentNode;
+
+    //ev.target.appendChild(document.getElementById(data));
+
+    ev.target.parentElement.childNodes.forEach(function (item, index) {
+        console.log(index);
+        if (item.id === targetItem.id) {
+            parentList.insertBefore(document.getElementById(data), parentList.childNodes[index]);
         }
     });
 }
