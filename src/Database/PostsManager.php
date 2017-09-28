acede33c754ae90e9f80998e49b2f18ecd33ae55
Robert Ogiba <robert.ogiba@outlook.com>
2017-09-28 22:03:22 +0200

    [PostView] HOT FIX!

--- src/Database/PostsManager.php
+++ src/Database/PostsManager.php
@@ -84,7 +84,7 @@ class PostsManager extends BaseDatabaseManager
                                                 u1.nickName AS modifiedBy
                                             FROM posts p 
                                             INNER JOIN users AS u ON p.author = u.id
-                                            INNER JOIN users AS u1 ON p.modifiedBy = u1.id
+                                            LEFT JOIN users AS u1 ON p.modifiedBy = u1.id
                                             WHERE p.id = ?");
         $stmt->bind_param("i", $id);
         $stmt->execute();
