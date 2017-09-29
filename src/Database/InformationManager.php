60d53d8b4a0c76a05e2aa22e6c9e3bb392b51f40
Robert Ogiba <ogiback@gmail.com>
2017-09-30 01:28:58 +0200

    Extended InformationManager class with method that allows to get section information by given section id

--- src/Database/InformationManager.php
+++ src/Database/InformationManager.php
@@ -13,8 +13,7 @@ use Model\About;
 use Model\Section;
 use Model\SectionInformation;
 
-class InformationManager extends BaseDatabaseManager
-{
+class InformationManager extends BaseDatabaseManager {
     const ABOUT_ME = 1;
 
     public function loadSections()
@@ -68,6 +67,30 @@ class InformationManager extends BaseDatabaseManager
             return $about;
         } else {
             $connection->close();
+            return null;
+        }
+    }
+
+    /**
+     *
+     * Load section information by given {@code id}
+     *
+     * @param $id
+     * @return SectionInformation|null
+     */
+    public function loadSectionById($id)
+    {
+        $connection = $this->createConnection();
+        $stmt = $connection->prepare("SELECT i.id, s.name AS section, i.content, i.image, i.dateTime FROM informations i INNER JOIN sections AS s ON i.section = s.id WHERE s.id = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+
+        $data = $this->bindResult($stmt);
+
+        if ($stmt->fetch()) {
+            $section = $this->arrayToObject($data, SectionInformation::class);
+            return $section;
+        } else {
             return null;
         }
     }
