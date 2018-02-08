1f9e3a809675d71ddbcbb5e69f705cc130185243
Robert Ogiba <ogiback@gmail.com>
2018-02-08 20:36:05 +0100

    Resotre navbar behavior after move to framework one

--- web/assets/scripts/post_view.js
+++ web/assets/scripts/post_view.js
@@ -3,16 +3,16 @@ window.addEventListener("DOMContentLoaded", function () {
 });
 
 function setupScrollListener() {
-    var navBar =  $('.nav-bar');
+    var navBar =  $("#nav-bar");
     var titleBar = $('.app-title');
     var distance = navBar.offset().top;
     var titleBarDistane = titleBar.offset().top;
 
     $(window).scroll(function() {
         if ( $(this).scrollTop() >= distance ) {
-            navBar.addClass('fixed');
+            navBar.addClass('fixed-top');
         } else {
-            navBar.removeClass("fixed");
+            navBar.removeClass("fixed-top");
         }
     });
 }
