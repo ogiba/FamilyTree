ea5b797ba64213076ff57f67e9750117c96e1d0d
Robert Ogiba <ogiback@gmail.com>
2018-05-19 01:47:28 +0200

    Provide api for testing action that allow to remove selected post from db

--- src/API/BaseRestController.php
+++ src/API/BaseRestController.php
@@ -20,9 +20,17 @@ abstract class BaseRestController {
         $this->serializeManager = new SerializeManager();
     }
 
-    protected function sendJsonResponse($data)
+    /**
+     * @param $data
+     * @param int $statusCode
+     */
+    protected function sendJsonResponse($data, $statusCode = StatusCode::OK)
     {
         header(ResponseHeaders::CONTENT_TYPE_JSON);
+        header("HTTP/1.1 " . StatusCode::getMessageForCode($statusCode));
+        if (is_null($data)) {
+            exit;
+        }
         echo json_encode($data);
     }
 
@@ -35,4 +43,6 @@ abstract class BaseRestController {
         header("HTTP/1.1 " . StatusCode::getMessageForCode($response->getStatusCode()));
         echo json_encode($response);
     }
+
+    abstract function action($name, $action, $params);
 }
--- src/API/PostController.php
+++ src/API/PostController.php
@@ -9,6 +9,8 @@ namespace API;
 
 use Database\PostsManager;
 use Model\PostPage;
+use Model\Response;
+use Utils\StatusCode;
 
 
 /**
@@ -16,10 +18,7 @@ use Model\PostPage;
  * @package Api
  *
  */
-
-
-class PostController extends BaseRestController
-{
+class PostController extends BaseRestController {
     const DEFAULT_PAGE_SIZE = 5;
     const STARTING_PAGE = 0;
 
@@ -35,28 +34,43 @@ class PostController extends BaseRestController
         $this->postsManager = new PostsManager();
     }
 
-    public function postListAction($request) {
+    function action($name, $action, $params)
+    {
+        switch ($action) {
+            case "":
+                if (!isset($_GET["id"])) {
+                    $this->postListAction($_REQUEST);
+                } else {
+                    $this->getPostAction($_REQUEST);
+                }
+                break;
+            case "remove":
+                $this->handleRemove();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public function postListAction($request)
+    {
         $pageSize = isset($request["pageSize"]) ? (int)$request["pageSize"] : 0;
-        $pageNumber = isset($request["page"]) ?  (int)$request["page"] : 0;
+        $pageNumber = isset($request["page"]) ? (int)$request["page"] : 0;
 
         $postPageSize = $pageSize != null && $pageSize > 0 ? $pageSize : self::DEFAULT_PAGE_SIZE;
 
         $postsPage = $this->postsManager->loadPosts($pageNumber, $postPageSize);
 
         if (count($postsPage->getPosts()) > 0) {
-            $jsonPage = $this->serializeManager->serializeJson($postsPage);
-
-            header("Content-Type: application/json");
-            header("HTTP/1.1 200 OK");
-            echo $jsonPage;
+            $this->sendJsonResponse($postsPage);
         } else {
-            header("HTTP/1.1 404 Not found");
-            echo "";
+            $this->sendJsonResponse("", StatusCode::NOT_FOUND);
         }
     }
 
-    function getPostAction($request) {
-        if (!isset($request["id"]) || empty($request["id"])){
+    function getPostAction($request)
+    {
+        if (!isset($request["id"]) || empty($request["id"])) {
             header("HTTP/1.1 422 Missing required parameter");
             echo "Required parameter not set";
             return;
@@ -65,7 +79,7 @@ class PostController extends BaseRestController
         $id = $request["id"];
         $selectedPost = $this->postsManager->loadPost($id);
 
-        if (!is_null($selectedPost)){
+        if (!is_null($selectedPost)) {
             $jsonString = $this->serializeManager->serializeJson($selectedPost);
             header("Content-Type: application/json");
             header("HTTP/1.1 200 OK");
@@ -73,6 +87,22 @@ class PostController extends BaseRestController
         } else {
             header("HTTP/1.1 404 Not found");
             echo "";
+        }
+    }
+
+    private function handleRemove()
+    {
+        if (!isset($_GET["id"])) {
+            $this->sendJsonResponse("", StatusCode::BAD_REQUEST);
+            exit;
+        }
+
+        $isRemoved = $this->postsManager->removePost($_GET["id"]);
+
+        if ($isRemoved) {
+            $this->sendJsonResponse("", StatusCode::OK);
+        } else {
+            $this->sendJsonResponse(null, StatusCode::UNPROCESSED_ENTITY);
         }
     }
 }
