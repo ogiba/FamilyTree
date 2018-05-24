d7bc516ebd061a07a472fd43d54ea112768dd67b
Robert Ogiba <ogiback@gmail.com>
2018-05-24 22:43:07 +0200

    [CookiesInfo] Add method that update cookies after closing alert to avoid displating it for the same user

--- web/assets/scripts/index.js
+++ web/assets/scripts/index.js
@@ -4,20 +4,27 @@
 
 window.addEventListener("DOMContentLoaded", function () {
     setupScrollListener();
+    setupAlertListener();
 });
 
 function setupScrollListener() {
-    var navBar =  $('#nav-bar');
+    var navBar = $('#nav-bar');
     var titleBar = $('.app-title');
     var distance = navBar.offset().top;
     var titleBarDistane = titleBar.offset().top;
 
-    $(window).scroll(function() {
-        if ( $(this).scrollTop() >= distance ) {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= distance) {
             navBar.addClass("fixed-top");
         } else {
             navBar.removeClass("fixed-top");
         }
+    });
+}
+
+function setupAlertListener() {
+    $("#cookieInfo").on("close.bs.alert", function () {
+        updateCookieInfo();
     });
 }
 
@@ -28,3 +35,25 @@ function selectPage(pageNumber) {
     });
 }
 
+function updateCookieInfo() {
+    setCookies("cookieInfo", "displayed");
+}
+
+
+function getCookie(key) {
+    let cookies = document.cookie;
+
+    if (!cookies.includes(key)) {
+        return null;
+    }
+
+    let values = cookies.split(";");
+    let cookieValue = values.find(t => t.trim().startsWith(key));
+    return cookieValue.replace(key, "");
+}
+
+function setCookies(key, value) {
+    document.cookie = `${key}=${value}`;
+
+    return document.cookie.includes(key);
+}
