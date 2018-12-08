341fdd613fce770d662ab5da5a129ae06ac068e5
Robert Ogiba <ogiback@gmail.com>
2018-12-08 01:56:21 +0100

    Create email format validation

--- www/web/assets/scripts/login_page.js
+++ www/web/assets/scripts/login_page.js
@@ -1,49 +1,74 @@
 /**
  * Created by ogiba on 25.09.2017.
  */
-(function()
-{
-    function submit() {
+(function() {
+	function submit() {
+		var loginInfoField = $("#loginInfo");
 
-        var loginInfoField = $("#loginInfo");
+		if (loginInfoField.hasClass("shown")) {
+			loginInfoField.removeClass("shown");
+		}
 
-        if (loginInfoField.hasClass("shown")){
-            loginInfoField.removeClass("shown");
-        }
+		$(".progress").addClass("shown");
 
-        $(".progress").addClass("shown");
+		$.ajax({
+			url: "login/loginUser",
+			type: "POST",
+			data: {
+				username: $("#usernameInput").val(),
+				password: $("#passwordInput").val()
+			},
+			success: function(data) {
+				window.location.href = "/admin";
+			},
+			error: function(error) {
+				var resp = JSON.parse(error.responseText);
 
-        $.ajax({
-            url: "login/loginUser",
-            type: 'POST',
-            data: {
-                "username":  $("#usernameInput").val(),
-                "password": $("#passwordInput").val()
-            },
-            success: function(data){
-                window.location.href = "/admin";
-            },
-            error: function (error) {
-                var resp = JSON.parse(error.responseText);
+				$(".progress").removeClass("shown");
 
-                $(".progress").removeClass("shown");
+				var loginInfoField = $("#loginInfo");
+				loginInfoField.text(resp.message);
+				loginInfoField.addClass("shown");
+			}
+		});
+	}
 
-                var loginInfoField = $("#loginInfo");
-                loginInfoField.text(resp.message);
-                loginInfoField.addClass("shown");
-            }
-        })
-    }
+	/**
+	 * @param e
+	 */
+	function loginOnEnter(e) {
+		if (e.keyCode === 13) submit();
+	}
 
-    /**
-     * @param e
-     */
-    function loginOnEnter(e) {
-        if(e.keyCode === 13)
-            submit();
-    }
+	$("#submit").click(submit);
+	$("#usernameInput").keydown(loginOnEnter);
+	$("#passwordInput").keydown(loginOnEnter);
 
-    $('#submit').click(submit);
-    $("#usernameInput").keydown(loginOnEnter);
-    $("#passwordInput").keydown(loginOnEnter);
+	$("#usernameInput").on("input", function(e) {
+		var re = new RegExp(".{1,}@{1}.{1,}\\.{1}.{2,}");
+
+		setTimeout(function() {
+			let insertedValue = $("#usernameInput").val();
+
+			var loginInfoField = $("#loginInfo");
+
+			if (insertedValue.includes("@")) {
+				let isProperValue = re.test(insertedValue);
+				console.log(isProperValue);
+
+				if (!isProperValue && !loginInfoField.hasClass("shown")) {
+					loginInfoField.text("Email not properly formatted");
+					loginInfoField.addClass("shown");
+				} else if (isProperValue && loginInfoField.hasClass("shown")) {
+					loginInfoField.removeClass("shown");
+				}
+			} else {
+				console.log("Not a email");
+
+				if (loginInfoField.hasClass("shown")) {
+					loginInfoField.removeClass("shown");
+				}
+			}
+		}, 500);
+	});
 })();
